446530eca12bc85d9e243d59effdc7855bd3f56d
push-ebx <nikita.keyn@yandex.ru>
2024-09-30 23:52:47 +0300

    fix: setup.sh

--- setup.sh
+++ setup.sh
@@ -2,12 +2,12 @@
 
 SCRIPT_DIR=$(dirname "$(realpath "$0")")
 SCRIPT_NAME="metrics.py"
-CRON_JOB="* * * * * /usr/bin/python3 $SCRIPT_DIR/$SCRIPT_NAME >> /var/log/metrics.log 2>&1"
+CRON_JOB="* * * * * /usr/bin/python3 /root/performance-server-script/$SCRIPT_NAME >> /var/log/metrics.log 2>&1"
 
 if [ -f "$SCRIPT_DIR/requirements.txt" ]; then
     echo "Installing Python dependencies..."
     sudo pip3 install -r "$SCRIPT_DIR/requirements.txt"
-    sudo /usr/bin/python3 -m http.server 8080 --directory $SCRIPT_DIR
+    nohup python3 -m http.server 8888 --directory /root > /var/log/http_server.log 2>&1 &
 else
     echo "No requirements.txt found. Skipping Python dependencies installation."
 fi
